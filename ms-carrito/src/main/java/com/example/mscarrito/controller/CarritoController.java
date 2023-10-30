bd7b15362eb8bdc95db0b7eb9fcf743f6c9c972d
Mark-Pino <mark.pino@upeu.edu.pe>
2023-10-30 11:07:09 -0500

    config-data

--- ms-carrito/src/main/java/com/example/mscarrito/controller/CarritoController.java
+++ ms-carrito/src/main/java/com/example/mscarrito/controller/CarritoController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/carrito")
+@RequestMapping("/carritoItem")
 public class CarritoController {
 
     private final CarritoService carritoService;
