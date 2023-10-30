50d2da87d1bfcef32a59aa11495860ce2541a042
Mark-Pino <mark.pino@upeu.edu.pe>
2023-10-29 23:54:53 -0500

    ms-factura y ms-pago

--- ms-pago/src/main/java/com/example/mspago/controller/PagoController.java
+++ ms-pago/src/main/java/com/example/mspago/controller/PagoController.java
@@ -2,7 +2,9 @@ package com.example.mspago.controller;
 
 import com.example.mspago.entity.Pago;
 import com.example.mspago.service.PagoService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -28,6 +30,7 @@ public class PagoController {
         return pagoService.crearPago(pago);
     }
 
+    @CircuitBreaker(name = "pagoListarPorIdCB", fallbackMethod = "fallBackPagoListarPorIdCB")
     @GetMapping("/{id}")
     public Optional<Pago> obtenerPagoPorId(@PathVariable Integer id) {
         return pagoService.obtenerPagoPorId(id);
@@ -36,5 +39,11 @@ public class PagoController {
     @DeleteMapping("/{id}")
     public void vaciarPago(@PathVariable Integer id) {
         pagoService.vaciarPago(id);
+    }
+
+    private ResponseEntity<Pago> fallBackPagoListarPorIdCB(@PathVariable(required = true) Integer id, RuntimeException e) {
+        Pago pago = new Pago();
+        pago.setId(90000);
+        return ResponseEntity.ok().body(pago);
     }
 }
